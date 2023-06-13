0fe8f50910a14f9688765cb1bf4125a27c821124
Chiragsanikam <chiragsanikam@gmail.com>
2023-06-14 04:33:13 +0530

    logic done

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 let btnTranslate = document.querySelector("#btn-translate");
-let textInput = document.querySelector("#textInput")
+let textInput = document.querySelector("#txtInput")
 let outputDiv =document.querySelector("#output")
+let serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server, try again later")
+}
+
 
 function clicked(){
-    output.innerText= "hi chirag";
-    
 
+    let InputText=textInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(InputText))
+        .then(response => response.json())
+        .then(json => {
+            let translatedText= json.contents.translated
+            outputDiv.innerText=translatedText})
+        .catch(errorHandler)
 }
 
 

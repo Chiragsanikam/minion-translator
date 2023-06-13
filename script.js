7bb20420d2c10b29b8ceacebaa84f5024edc17d9
Chiragsanikam <chiragsanikam@gmail.com>
2023-06-14 02:48:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let btnTranslate = document.querySelector("#btn-translate");
+let textInput = document.querySelector("#textInput")
+let outputDiv =document.querySelector("#output")
+
+function clicked(){
+    output.innerText= "hi chirag";
+    
+
+}
+
+
+btnTranslate.addEventListener("click", clicked);
+
+
+
